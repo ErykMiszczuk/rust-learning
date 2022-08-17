e5291801521badee31f65395ee5e6122a67f9c71
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2022-08-17 22:05:51 +0200

    Hello world done

---
+++ hello_world/main.rs
@@ -0,0 +1,15 @@
+/** 
+ * main function is always the first code to run
+ * it returns nothing and takes no arguments
+ */
+fn main() {
+    // rust indentation style: 4 spaces
+
+    /**
+     * Here is rust macro 'println!' it prints text to screen
+     * macros are diffrent than standard functions
+     * 
+     * finish line with semicolon
+     */
+    println!("Hello, world!");
+}
