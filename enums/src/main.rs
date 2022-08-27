b5f8a8d0a2606d78a4cf3abdaaa713532f603c6d
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2022-08-27 23:12:36 +0200

    Finished if let

---
+++ enums/src/main.rs
@@ -0,0 +1,39 @@
+enum IpAddr {
+    V4(String),
+    V6(String)
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32)
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
+}
+
+fn main() {
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
+//This is also possible to put struct in enum variants
+// struct Ipv4Addr {
+//     // --snip--
+// }
+
+// struct Ipv6Addr {
+//     // --snip--
+// }
+
+// enum IpAddr {
+//     V4(Ipv4Addr),
+//     V6(Ipv6Addr),
+// }
