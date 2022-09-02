a0271e5e7fb37bf2112e7ff143c1886e838bb727
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2022-09-02 17:54:38 +0200

    Finished tests!

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,13 +2,121 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+    // String::from("Hello!")
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+}
+
+// We can run test one by one passing tests names as arguments for 'cargo test'
+// Also we can filter running test by part of test names eg 'smaller'
+// will run 2 tests
+
+// 'cfg(test)' tells compiler that this coded will be compiled only in test so for production
+// resulting program will not have tests included. This save space and compile time
+
+// private functions can be tested in rust without any problems
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // ignore attribute ignores running this test by 'cargo test'
+    // to run it we need to pass '--ignored' flag
+    // when running test - 'cargo test -- --ignored'
+    // to run all test we run 'cargo test -- --include-ignored'
     #[test]
-    fn it_works() {
+    #[ignore]
+    fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn greetings_contain_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
